eda922b9baee81876e5a341475042bae332fb7a1
javips02 <javierperezsalasar@gmail.com>
2024-12-08 13:00:42 +0100

    Primeros pasos. Kubernetes funcionando. No agnado srv y test porque todavia no funcionan dentro del cluster

--- p5/raft/buildP5.sh
+++ p5/raft/buildP5.sh
@@ -1,51 +1,59 @@
 #!/bin/bash
-
+echo "Iniciando clúster kind (si ya existe dará error)"
+./Deployment/kube/kind-with-registry.sh
 # Variables
-OUTPUT_DIR="./Deployment"
-TEST_BINARY_DIR="$OUTPUT_DIR"
-MAIN_BINARY_NAME="srvraft"
-MAIN_BINARY_PATH="./cmd/srvraft"
-GO_VERSION="1.23.3"
+SERVER_SRC="./cmd/srvraft"
+TESTS_SRC="./internal/testintegracionraft2"
+OUTPUT_DIR="./Deployment/bin"
+SERVER_BIN="$OUTPUT_DIR/server"
+TESTS_BIN_DIR="$OUTPUT_DIR/tests"
 
-# Crear directorios de salida si no existen
+# Crear directorios de salida
 mkdir -p "$OUTPUT_DIR"
-mkdir -p "$TEST_BINARY_DIR"
+mkdir -p "$TESTS_BIN_DIR"
 
-# Compilar el ejecutable principal
-echo "Compilando el ejecutable principal..."
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o "$OUTPUT_DIR/$MAIN_BINARY_NAME" "$MAIN_BINARY_PATH"
-
-if [ $? -ne 0 ]; then
-    echo "Error: No se pudo compilar el ejecutable principal."
+# Compilación estática del servidor
+echo "Compilando servidor de forma estática..."
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags '-extldflags "-static"' -o "$SERVER_BIN" "$SERVER_SRC"
+if [[ $? -eq 0 ]]; then
+    echo "Servidor compilado exitosamente en $SERVER_BIN"
+else
+    echo "Error al compilar el servidor"
     exit 1
 fi
-echo "Ejecutable principal compilado en $OUTPUT_DIR/$MAIN_BINARY_NAME"
 
-# Compilar los tests
-echo "Compilando los tests..."
-for TEST_FILE in $(find ./internal -name '*_test.go'); do
-    TEST_NAME=$(basename "$TEST_FILE" .go)
-    TEST_DIR=$(dirname "$TEST_FILE")
-    OUTPUT_TEST_BINARY="$TEST_BINARY_DIR/$TEST_NAME"
-
-    CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go test -c -o "$OUTPUT_TEST_BINARY" "$TEST_DIR"
-
-    if [ $? -ne 0 ]; then
-        echo "Error: No se pudo compilar el test $TEST_FILE."
+# Compilación estática de los tests
+echo "Compilando tests de forma estática..."
+for test_file in "$TESTS_SRC"/*_test.go; do
+    test_name=$(basename "$test_file" .go)
+    test_bin="$TESTS_BIN_DIR/$test_name"
+    CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go test -c -o "$test_bin" "$test_file"
+    if [[ $? -eq 0 ]]; then
+        echo "Test $test_name compilado exitosamente en $test_bin"
+    else
+        echo "Error al compilar el test $test_name"
         exit 1
     fi
-    echo "Test compilado: $OUTPUT_TEST_BINARY"
 done
 
-chmod +x ./Deployment/srvraft
-chmod +x ./Deployment/testintegracionraft1_test
+echo "Compilación completada. Binarios guardados en $OUTPUT_DIR"
+echo "Moviendo binarios a directorios de Dockerfile"
+chmod +x ./Deployment/bin/server && mv ./Deployment/bin/server ./Deployment/kube/Dockerfiles/servidor/servidor
+chmod +x ./Deployment/bin/tests/testintegracionraft1_test && mv ./Deployment/bin/tests/testintegracionraft1_test ./Deployment/kube/Dockerfiles/cliente/cliente
 
-mv ./Deployment/srvraft ./Deployment/kube/Dockerfiles/servidor/servidor
-mv ./Deployment/testintegracionraft1_test ./Deployment/kube/Dockerfiles/cliente/cliente
+echo "Generando imágenes y subiéndolas al repositorio"
+(
+    cd ./Deployment/kube/Dockerfiles/servidor &&
+    docker build . -t localhost:5001/servidor:latest &&
+    docker push localhost:5001/servidor:latest
+)
+(
+    cd ./Deployment/kube/Dockerfiles/cliente &&
+    docker build . -t localhost:5001/cliente:latest &&
+    docker push localhost:5001/cliente:latest
+)
 
-echo "Todos los ejecutables y tests han sido compilados con éxito."
-
-echo "COMENZANDO A DOCKERIZAR EL PROYECTO"
-cd ./Deployment/kube/Dockerfiles/servidor && docker build . -t localhost:5000/servidor:latest && docker push localhost:5000/servidor:latest 
-cd ./Deployment/kube/Dockerfiles/cliente && docker build . -t localhost:5000/cliente:latest && docker push localhost:5000/cliente:latest
-
+echo "Creando statefulSet dentro del cluster kind"
+kubectl apply -f statefulset_go.yaml
+sleep 2
+kubectl get pods --all-namespaces -o wide
--- p5/raft/internal/testintegracionraft2/testintegracionraft1_test.go
+++ p5/raft/internal/testintegracionraft2/testintegracionraft1_test.go
@@ -18,9 +18,9 @@ import (
 
 const (
 	//nodos replicas
-	REPLICA1 = "127.0.0.1:29001"
-	REPLICA2 = "127.0.0.1:29002"
-	REPLICA3 = "127.0.0.1:29003"
+	REPLICA1 = "ss-0.ss-service.default.svc.cluster.local:6000"
+	REPLICA2 = "ss-1.ss-service.default.svc.cluster.local:6000"
+	REPLICA3 = "ss-2.ss-service.default.svc.cluster.local:6000"
 
 	// paquete main de ejecutables relativos a directorio raiz de modulo
 	EXECREPLICA = "cmd/srvraft/main.go"
@@ -31,7 +31,7 @@ const (
 
 // PATH de los ejecutables de modulo golang de servicio Raft
 // var PATH string = filepath.Join(os.Getenv("HOME"), "tmp", "p3", "raft")
-var PATH string = "/home/conte/Desktop/Lezioni/SSDD/SISDIS_practicas_2425_g37/p4/raft/"
+var PATH string = "/home/javi/github/SISDIS_practicas_2425_g37/p5/raft/"
 
 // go run cmd/srvraft/main.go 0 127.0.0.1:29001 127.0.0.1:29002 127.0.0.1:29003
 var EXECREPLICACMD string = "cd " + PATH + "; go run " + EXECREPLICA
